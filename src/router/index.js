da3800fa8874247fdaf074e7f10bd871290e300c
Awais Javaid <53362912+awaisjavaid930@users.noreply.github.com>
2023-01-27 16:20:26 +0500

    Register Form Setting

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+    createRouter,
+    createWebHistory
+} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: () => import('../views/AboutView.vue')
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: () => import('../views/auth/RegisterView.vue')
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/auth/LoginView.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,6 @@
 
 <script>
 import { defineComponent } from 'vue';
-
-// Components
 import HelloWorld from '../components/HelloWorld.vue';
 
 export default defineComponent({
