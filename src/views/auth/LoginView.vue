ae19a1c257a3b2035901c7ad84c1b260aead9a2a
Awais Javaid <53362912+awaisjavaid930@users.noreply.github.com>
2023-01-30 11:40:21 +0500

    Setting Up the Layout of Login, Register Form

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,10 +1,41 @@
 <template>
-    <div>
-        Login
-    </div>
+    <v-container>
+        <v-row justify="space-around">
+            <v-card class="px-5" width="400">
+                <h3 class="font-weight-bold text-center pb-5" justify="center" size="meduim">
+                    <v-img max-height="150" max-width="250" src="@/assets/cycle.gif"></v-img>
+                </h3>
+                <form @submit.prevent="login">
+                    <v-text-field v-model="formData.email" label="E-mail">
+                    </v-text-field>
+                    <v-text-field type="password" dense outlined v-model="formData.password" label="Password">
+                    </v-text-field>
+
+                    <v-btn type="submit" color="primary" class="mb-5 mr-4" @click="submit">
+                        Login
+                    </v-btn>
+                    <v-btn to="/register" class="mb-5" depressed color="error">
+                        Register
+                    </v-btn>
+                </form>
+            </v-card>
+        </v-row>
+    </v-container>
 </template>
 <script>
-    export default {
-        name : 'LoginView'
-    }
+export default {
+    name: 'LoginView',
+    data:() => ({
+        formData : {
+            email : '',
+            password : '' 
+        }
+    }),
+    methods: {
+        login()
+        {
+            console.log(this.formData)  
+        }
+    },
+}
 </script>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,65 +1,44 @@
 <template>
     <v-container>
         <v-row justify="space-around">
-            <v-card width="400">
-                <v-img height="200px" src="https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg">
-                    <v-app-bar flat color="rgba(0, 0, 0, 0)">
-                        <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon>
-
-                        <v-toolbar-title class="text-h6 white--text pl-0">
-                            Messages
-                        </v-toolbar-title>
-
-                        <v-spacer></v-spacer>
-
-                        <v-btn color="white" icon>
-                            <v-icon>mdi-dots-vertical</v-icon>
-                        </v-btn>
-                    </v-app-bar>
-
-                    <v-card-title class="white--text mt-8">
-                        <v-avatar size="56">
-                            <img alt="user"
-                                src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg">
-                        </v-avatar>
-                        <p class="ml-3">
-                            John Doe
-                        </p>
-                    </v-card-title>
-                </v-img>
-
-                <v-card-text>
-                    <div class="font-weight-bold ml-8 mb-2">
-                        Today
-                    </div>
-
-                    <v-timeline align-top dense>
-                        <v-timeline-item v-for="message in messages" :key="message.time" :color="message.color" small>
-                            <div>
-                                <div class="font-weight-normal">
-                                    <strong>{{ message.from }}</strong> @{{ message.time }}
-                                </div>
-                                <div>{{ message.message }}</div>
-                            </div>
-                        </v-timeline-item>
-                    </v-timeline>
-                </v-card-text>
+            <v-card class="px-5" width="400">
+                <h3 class="font-weight-bold text-center pb-5" justify="center" size="meduim">
+                    <v-img max-height="150" max-width="250" src="@/assets/cycle.gif"></v-img>
+                </h3>
+                <form  @submit.prevent="register">
+                    <v-text-field v-model="formData.name" :counter="10" label="Name">
+                    </v-text-field>
+                    <v-text-field v-model="formData.email" label="E-mail">
+                    </v-text-field>
+                    <v-text-field type="password" dense outlined v-model="formData.password" label="Password">
+                    </v-text-field>
+                    
+                    <v-btn type="submit" color="primary" class="mb-5 mr-4" @click="submit">
+                        Register
+                    </v-btn>
+                    <v-btn to="/login" class="mb-5" depressed color="error">
+                        Login
+                    </v-btn>
+                </form>
             </v-card>
         </v-row>
     </v-container>
 </template>
+
 <script>
 export default {
     name: 'RegisterView',
     data: () => ({
-        messages: [
-            {
-                from: 'You',
-                message: `Sure, I'll see you later.`,
-                time: '10:42am',
-                color: 'deep-purple lighten-1',
-            }
-        ],
+        formData: {
+            name: '',
+            email: '',
+            password : ''
+        }
     }),
+    methods: {
+        register() {
+            console.log(this.formData)
+        },
+    },
 }
 </script>
